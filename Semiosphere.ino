b188cb0600ef79d0b47870ba4a1b23942d5ab803
Jonas Johansson <jns.johansson@gmail.com>
2019-05-08 11:26:31 +0200

    added variable for chipset, datapin, increased brightness, slowed down animation

--- Semiosphere.ino
+++ Semiosphere.ino
@@ -1,15 +1,18 @@
 #include<FastLED.h>
 #include "palettes.h"
 
-#define NUM_LEDS 60
-#define BRIGHTNESS 140
+#define CHIPSET UCS1903
+#define DATA_PIN 6
+#define NUM_LEDS 30
+#define BRIGHTNESS 255 // 0-255
 
 CRGB leds[NUM_LEDS];
 
 void setup() {
   FastLED.setBrightness(BRIGHTNESS);
-  FastLED.addLeds<UCS1903, 6>(leds, NUM_LEDS);
+  FastLED.addLeds<CHIPSET, DATA_PIN>(leds, NUM_LEDS);
   FastLED.clear();
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -17,9 +20,11 @@ void loop() {
     CRGBPalette16 bg_p( backgrounds_p[1] );
 
     for (int i = 0; i < NUM_LEDS; i++) {
-      int index = cos8( millis() / 20 + i);
+      
+      int index = cos8( millis() / 100 + i);
       CRGB color = ColorFromPalette(bg_p, index, 255, LINEARBLEND);
       leds[i] = color;
     }
+    
   FastLED.show();
 }
